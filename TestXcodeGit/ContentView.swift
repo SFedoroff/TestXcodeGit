cc296b3d0e857d907d7905177a79116e851505ad
Vyacheslav Fedorov <slava.fedoroff@mail.ru>
2020-01-25 23:35:29 +0300

    Change Text

---
+++ TestXcodeGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestXcodeGit
+//
+//  Created by Vyacheslav on 25.01.2020.
+//  Copyright © 2020 Business Systems. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .bold()
+        }
+    
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
